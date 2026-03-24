7607cd6a073eb515c361087aac11a20032f39ff2
rehanrkhan11 <rehanrazakhan91@gmail.com>
2026-03-24 16:40:53 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,11 @@
+// api/index.js
 const app = require("../server");
+const connectDB = require("../config/db");
 
-module.exports = app;
+// This ensures the DB is connected for every serverless execution
+const handler = async (req, res) => {
+  await connectDB();
+  return app(req, res);
+};
+
+module.exports = handler;
